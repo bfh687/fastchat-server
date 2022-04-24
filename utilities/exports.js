2755c92a498fca26e9122d8f980c40689972cdda
bfh687 <bfh687@uw.edu>
2022-04-23 19:13:51 -0700

    fix misnamed credential import

--- utilities/exports.js
+++ utilities/exports.js
@@ -1,9 +1,10 @@
 const pool = require("./sql_conn.js");
-const cred = require("./credentialing");
+const cred = require("./cred.js");
+const validation = require("./validation.js");
+
+const sendEmail = require("./email.js").sendEmail;
 const generateHash = cred.generateHash;
 const generateSalt = cred.generateSalt;
-const validation = require("./validationUtils.js");
-const sendEmail = require("./email.js").sendEmail;
 
 module.exports = {
     pool,
