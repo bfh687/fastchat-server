e9c05d8d80285534d8fe024fd161af0be401c4f9
bfh687 <bfh687@uw.edu>
2022-05-16 19:48:32 -0700

    Update weather.js

--- routes/weather.js
+++ routes/weather.js
@@ -1,45 +1,62 @@
-const express= require('express')
-const https = require('https')
+const express = require("express");
+const https = require("https");
 
 // Initiate the route
-var router= express.Router()
+var router = express.Router();
 
-const bodyParser= require('body-parser')
-router.use(bodyParser.urlencoded({extended: true}))
-//require('dotenv').config()
-//const apiKey = `${process.env.WEATHER_API}`;
+const bodyParser = require("body-parser");
+router.use(bodyParser.urlencoded({ extended: true }));
 
-router.get("/zipcode/:code",(req, res) => {
-    let zip = req.params.code
-    let unit = "imperial"
-    console.log(zip)
+router.get("/zipcode/:code", (req, res) => {
+  let zip = req.params.code;
+  let unit = "imperial";
+  console.log(zip);
 
-    const url= "https://api.openweathermap.org/data/2.5/weather?zip="+zip+",us&units="+unit+"&appid="+process.env.WEATHER_API          
-        if (zip) {
-            https.get(url, (response)=>{
-                response.on('data', (data)=>{
-                    const weather=JSON.parse(data)
-                    let info = weather.main.temp;
-                    res.send({
-                       zipcode: info                     
-                    })
-                })
-    
-            })
-        } else {
-            response.status(400).send({
-                message: "Missing required information"
-            })
-        }
+  const url = "https://api.openweathermap.org/data/2.5/weather?zip=%22+zip+%22,us&units=%22+unit+%22&appid=%22+process.env.WEATHER_API";
+  if (zip) {
+    https.get(url, (response) => {
+      response.on("data", (data) => {
+        const weather = JSON.parse(data);
+        let info = weather.main.temp;
+        res.send({
+          info,
+        });
+      });
+    });
+  } else {
+    response.status(400).send({
+      message: "Missing required information",
+    });
+  }
 });
-      /*  if(err){
-          console.log('error:', error);
-        } else {
-            let weather = JSON.parse(body);
-            let info = `Data for ${weather.main.temp}`;
-           
-          res.send({ 'body': info });
-        }
-        */
-    
-module.exports = router
+
+//hourly weather using zipcode
+router.get("/daily/:code", (req, res) => {
+  let query = req.params.code;
+  console.log(query);
+  let unit = "imperial";
+  console.log("1");
+  const url = "https://api.openweathermap.org/data/2.5/forecast?zip=%22+query+%22,us&appid=%22+process.env.WEATHER_API";
+  console.log("2");
+  if (query) {
+    console.log("hi");
+
+    https.get(url, (response) => {
+      console.log("pls");
+      response.on("data", (data) => {
+        console.log("okay here");
+        const weather = JSON.parse(data);
+        console.log("3");
+
+        res.send({
+          weather,
+        });
+        console.log("4");
+      });
+    });
+  } else {
+    response.status(400).send({ message: "Missing required information" });
+  }
+});
+
+module.exports = router;
