4e23a9e08b1865f942721871bb4bbc0d35fc1d4f
bfh687 <bfh687@uw.edu>
2022-05-18 16:00:52 -0700

    adds current weather endpoint + update apidocs

--- routes/weather.js
+++ routes/weather.js
@@ -343,4 +343,142 @@ router.get("/hourly", (req, res) => {
     });
 });
 
+/**
+ * @api {get} /weather/current/:zipcode Request to get current forecast via zipcode
+ * @apiName CurrentWeatherZipcode
+ * @apiGroup Weather
+ *
+ * @apiParam {String} zipcode the zipcode to request the forecast for
+ *
+ * @apiSuccess {Object} data The current forecast for the given zipcode
+ *
+ *  * @apiSuccessExample {json} Success-Response:
+ *     HTTP/1.1 201 OK
+ *     {
+ *        "coords": {
+ *          "latitude": 47.671346,
+ *          "longitude": -122.34166
+ *        },
+ *        "current": {
+ *            "date": "2022-05-18",
+ *            "time": "15:28:02",
+ *            "temp": 61,
+ *            "desc": "Clear",
+ *            "type": "wind"
+ *        }
+ *     }
+ *
+ * @apiError (400: Error Retrieving Weather Data) {String} message "Error Retrieving Weather Data"
+ *
+ */
+router.get("/current/:zipcode", (req, res) => {
+  fetch(
+    `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${req.params.zipcode}/today?unitGroup=us&include=current&key=${process.env.WEATHER_API}&contentType=json`,
+    {
+      method: "GET",
+      headers: {},
+    }
+  )
+    .then((result) => result.json())
+    .then((result) => {
+      const data = new Object();
+
+      data.coords = {
+        latitude: result.latitude,
+        longitude: result.longitude,
+      };
+
+      const current = new Object();
+      current.date = result.days[0].datetime;
+      current.time = result.currentConditions.datetime;
+      current.temp = result.currentConditions.temp;
+      current.desc = result.currentConditions.conditions;
+      current.type = result.currentConditions.icon;
+
+      data.current = current;
+
+      res.status(200).send(data);
+    })
+    .catch((err) => {
+      res.status(400).send({
+        message: "Error Retrieving Weather Data",
+        error: error,
+      });
+    });
+});
+
+/**
+ * @api {get} /weather/current?lat=LAT&long=LONG  Request to get current forecast via lat/long
+ * @apiName CurrentWeatherLatLong
+ * @apiGroup Weather
+ *
+ * @apiParam {String} lat the latitude to request the forecast for
+ * @apiParam {String} long the longitude to request the forecast for
+ *
+ * @apiSuccess {Object} data The current forecast for the given lat/long
+ *
+ *  * @apiSuccessExample {json} Success-Response:
+ *     HTTP/1.1 201 OK
+ *     {
+ *        "coords": {
+ *          "latitude": 47.6062,
+ *          "longitude": -122.3321
+ *        },
+ *        "current": {
+ *            "date": "2022-05-18",
+ *            "time": "15:38:18",
+ *            "temp": 57,
+ *            "desc": "Clear",
+ *            "type": "clear-day"
+ *        }
+ *     }
+ *
+ * @apiError (400: Missing Query Parameters) {String} message "Missing Required Query Parameters"
+ * @apiError (400: Error Retrieving Weather Data) {String} message "Error Retrieving Weather Data"
+ *
+ */
+router.get("/current", (req, res) => {
+  if (!req.query.lat || !req.query.long) {
+    res.status(400).send({
+      message: "Missing Required Query Parameters",
+      error: error,
+    });
+    return;
+  }
+
+  fetch(
+    `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${req.query.lat}%2C${req.query.long}/today?unitGroup=us&include=current&key=${process.env.WEATHER_API}&contentType=json`,
+    {
+      method: "GET",
+      headers: {},
+    }
+  )
+    .then((result) => result.json())
+    .then((result) => {
+      const data = new Object();
+
+      data.coords = {
+        latitude: result.latitude,
+        longitude: result.longitude,
+      };
+
+      const current = new Object();
+      current.date = result.days[0].datetime;
+      current.time = result.currentConditions.datetime;
+      current.temp = result.currentConditions.temp;
+      current.desc = result.currentConditions.conditions;
+      current.type = result.currentConditions.icon;
+
+      data.current = current;
+
+      res.status(200).send(data);
+    })
+    .catch((err) => {
+      res.status(400).send({
+        message: "Error Retrieving Weather Data",
+        error: error,
+      });
+    });
+});
+
 module.exports = router;
